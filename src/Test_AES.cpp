eac53ad21f3fdd9c228ba2f9b974b784e20a6f40
Ryan Portocarrero <portocarrero.ryan@gmail.com>
2022-11-05 17:28:07 -0400

    Update Test_AES.cpp
    added reading test vectors

--- src/Test_AES.cpp
+++ src/Test_AES.cpp
@@ -1,12 +1,35 @@
 #include "AES.cpp"
 #include <fstream>
+#include <iostream>
+#include <cstring>
+#include <stdio.h>
 
-using namesapce std;
+using namespace std;
+
+const char delim[2] = " ";
 
 int main(){
+    char key[128];
+    char plaintext[128];
+    char ciphertext[128];
 
     fstream newFile;
-    newFile.open("GFSBox_KAT_128");
+    newFile.open("../test/GFSBox_KAT_128");
+    if(newFile.is_open()){
+        string line;
+        getline(newFile,line);
+        strcpy(key, line.c_str());
+
+        while(newFile >> plaintext >> ciphertext){
+            cout << "Key: "<< key << endl;
+            cout << "Plaintext: " <<  plaintext << endl;
+            cout << "Ciphertext: " << ciphertext << endl;
+            cout << endl;
+        }
+        newFile.close();
+    }
     
+    else cout << "Unable to open File\n";
+
     return 0;
 }
