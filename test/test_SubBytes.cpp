cc137fe561f258a4ad4db0805ca235e70530039f
Ryan Portocarrero <portocarrero.ryan@gmail.com>
2022-11-09 17:22:13 -0500

    Update test_SubBytes.cpp

--- test/test_SubBytes.cpp
+++ test/test_SubBytes.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-TEST(SubBytes, test1)
+TEST(SubBytes_test, test1)
 {
     ifstream infile("test_SubBytes_test1.hex");
     char input[16];
     infile.read(input, 16);
     char expected[16];
     infile.read(expected,16);
-    //SubBytes(input);
+    SubBytes(input);
     ASSERT_EQ(input, expected);
 }
